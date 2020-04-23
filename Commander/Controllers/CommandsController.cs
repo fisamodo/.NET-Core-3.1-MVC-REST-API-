c5eb8aa479b7cb8fab984f7e3aae0067e7132528
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-04-23 11:50:52 +0200

    added controller and made GetAll and getById commands. Tested with postman

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Commander.Models;
+using System.Collections.Generic;
+using Commander.Data;
+
+namespace Commander.Controllers
+{
+    
+    [Route("api/commands")]  
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+    
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
