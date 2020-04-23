82d6a72db6c030190bbc5942fdd1787b510b8bb8
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-04-23 11:27:36 +0200

    adding a "mock" repository and implementing interface

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil", Line="Boil", Platform="Kettle"},
+                new Command{Id=1, HowTo="Cut", Line="On Desk", Platform="Board"},
+                new Command{Id=2, HowTo="Dinst", Line="Kapula", Platform="Tava"}
+            };
+
+            return commands;
+        }
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil", Line="Boil", Platform="Kettle"};
+        }
+    }
+}
