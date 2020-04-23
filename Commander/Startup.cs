c5a39ef8ca544560e6ad2f73130a6877a2aad51b
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-04-23 14:08:33 +0200

    postman testing for getall and getbyid works

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -32,7 +32,9 @@ namespace Commander
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            //services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
